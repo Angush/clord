7772bed6ec0856ce6f6397f8d3bb4f69ea175ffb
Shane <40532070+Shane-West@users.noreply.github.com>
2018-08-02 11:05:45 +1000

    Update MVF_1_v2.cljs
    Added docstring for get_msg_info
    Removed some print statements

--- src/server/MVF_1_v2.cljs
+++ src/server/MVF_1_v2.cljs
@@ -6,7 +6,7 @@
 
 
 (defn mvf_1
-  [channel_id msg_id content author]
+  [msg channel_id msg_id content author]
 
   ; TODO:
   ; Read blacklist from file and convert to set
@@ -31,30 +31,31 @@
   ;(println (type msg_words) msg_words)
 
   ; Test var + debug print
-  ; (def has_bad_words (empty?(set/intersection blacklist msg_words)) )
-  ; (println has_bad_words)
+  ;(def has_bad_words (empty?(set/intersection blacklist msg_words)) )
+  ;(println has_bad_words)
 
   ; Get result of intersection of blacklist and msg words sets
   (if-not (empty?(set/intersection blacklist msg_words))
+  
+    (println "The message has bad words")
     ; TODO
     ; Remove message
-
+  
     ; TODO
     ;Post warning message
-
-    (println
-      (format "Hey @%s! Wash your mouth out with soap! We don't use that kind of language here."
-              author)
-      )
-
+    ;(.reply msg " wash your mouth out with soap! We don't use that kind of language here.")
+  
     ; TODO
     ; Call auto punishment mvf
-
+  
     )
   )
 
 
 (defn get_msg_info
+  "This function accepts msg object from discord and gets the message ID, channel ID,
+   author, and message content. It then passes this info to mvf_1"
+
   [msg]
 
   (def channel_id (aget msg "channel"))
@@ -62,22 +63,8 @@
   (def content (aget msg "content"))
   (def author (aget msg "author"))
 
-  ; Debug prints
-  ;(println (type channel_id) channel_id)
-  ;(println (type msg_id) msg_id)
-  ;(println (type content) content)
-  ;(println (type author) author)
-
-  (mvf_1 channel_id msg_id content author)
+  (mvf_1 msg channel_id msg_id content author)
   )
-
-
-; Run mvf_1 directly for testing
-(println "-----")
-(mvf_1 10 20 "This message contains shit" "Bob")
-(println "-----")
-(mvf_1 10 20 "This message contains words" "Bob")
-(println "-----")
 
 
 ; TODO Bot permissions:
