d694766a8733b222c8ac57a4636c8862430a7595
Shane <40532070+Shane-West@users.noreply.github.com>
2018-08-03 13:09:11 +1000

    Update MVF_5.cljs
    Can load the blacklist

--- src/server/MVF_5.cljs
+++ src/server/MVF_5.cljs
@@ -1,20 +1,29 @@
 (ns server.MVF_5
   (:require [clojure.string :as str]
+            ["fs" :as fs]
+          ))
 
-            )
-  )
+; I canabalised this from repository.cljs, I might move this to repository later.
+(defn read_blacklist
+  []
+  (def filename "src/blacklist.json")
+  (js->clj (.parse js/JSON (fs/readFileSync filename "utf8")) :keywordize-keys true))
 
 
 (defn add_term
-      []
-      (println "Adding to blacklist"))
+  []
+  ;Load blacklist, _x added to avoid warning. I'll probably change this later
+  (def blacklist_a (read_blacklist))
+  (println "Adding to blacklist"))
 
 
 (defn remove_term
-      []
-      (println "Removing from blacklist"))
+  []
+  (def blacklist_b (read_blacklist))
+  (println "Removing from blacklist"))
 
 
 (defn view_terms
-      []
-      (println "Dumping blacklist"))
+  []
+  (def blacklist_c (read_blacklist))
+  (println "Dumping blacklist"))
