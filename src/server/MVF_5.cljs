bb85964673b8ed1fa98bb7b5606657b3923c8a57
Shane <40532070+Shane-West@users.noreply.github.com>
2018-08-05 09:06:52 +1000

    Update MVF_5.cljs
    Moved blacklist loading to repository

--- src/server/MVF_5.cljs
+++ src/server/MVF_5.cljs
@@ -1,29 +1,26 @@
 (ns server.MVF_5
   (:require [clojure.string :as str]
-            ["fs" :as fs]
+            [server.repository :as repo]
           ))
-
-; I canabalised this from repository.cljs, I might move this to repository later.
-(defn read_blacklist
-  []
-  (def filename "src/blacklist.json")
-  (js->clj (.parse js/JSON (fs/readFileSync filename "utf8")) :keywordize-keys true))
 
 
 (defn add_term
   []
   ;Load blacklist, _x added to avoid warning. I'll probably change this later
-  (def blacklist_a (read_blacklist))
-  (println "Adding to blacklist"))
+  (def blacklist_a (repo/read_blacklist))
+      (println (type blacklist_a))
+      (println "Adding to blacklist"))
 
 
 (defn remove_term
   []
-  (def blacklist_b (read_blacklist))
-  (println "Removing from blacklist"))
+  (def blacklist_b (repo/read_blacklist))
+      (println (type blacklist_b))
+      (println "Removing from blacklist"))
 
 
 (defn view_terms
   []
-  (def blacklist_c (read_blacklist))
+  (def blacklist_c (repo/read_blacklist))
+  (println (type blacklist_c))
   (println "Dumping blacklist"))
