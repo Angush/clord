75e42a50cd9afc00a80150f74ea07fddddedcfb7
Shyam Nath <Sharminator@users.noreply.github.com>
2018-08-08 13:46:49 +1000

    :arrow_up: Added Transit for JSON.

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject clord "0.0.0"
   :dependencies
   [[thheller/shadow-cljs "2.4.10"]
+   [com.cognitect/transit-cljs "0.8.256"]
    [org.clojure/clojurescript "1.10.339"]]
 
   :source-paths
