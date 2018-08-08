73c2f29daa7a5282a4b94e8086e4acef242c8ec2
Shane <40532070+Shane-West@users.noreply.github.com>
2018-08-08 10:55:47 +1000

    Completed MVF_5.cljs
    Fully functional.
    
    Commands: .addterm =<term>, .removeterm =<term>, .viewterms
    <term> should be replace with the desired term when running. 
    
    Can view the blacklist, add to and remove from the blacklist.
    
    If the command is entered incorrectly, the bot will reply saying so.

--- src/server/MVF_5.cljs
+++ src/server/MVF_5.cljs
@@ -6,61 +6,67 @@
             [goog.string.format]))
 
 (defn add_term
+  "When called with '.addterm =<term>' this function will add the given <term> to
+   the blacklist if it's not currently blacklisted"
   [msg]
-  ;Load blacklist
+
+  ; Load blacklist
   (def blacklist (into #{} (repo/read_blacklist)))
 
-  ; Hardcoded test
-  ;(def msgt ".addterm =Chocolate")
-  ;(def content (str/lower-case msgt))
-  ;(println content)
-
   ; Get content from msg
-  (def content (str/lower-case (aget msg "content")))
+  (def content (re-find #"=\S+" (str/lower-case (aget msg "content"))))
 
-  ; Find term to be added from message content
-  (def new_term (str/replace (re-find #"=\S+" content) #"=" ""))
-
-  ; Check if term is not blacklist
-  (if-not (contains? blacklist new_term)
-    ; Term not blacklisted
+  (if (some? content)
+    ; If command is properly formatted
     (do
-      ; Add term to blacklist and sort alphabetically before writing file
-      (repo/write-data (sort(conj blacklist new_term)))
-      ; Reply with success message
-      (.reply msg "the blacklist has been updated!"))
+      ; Strip new term down to word only
+      (def new_term (str/replace content #"=" ""))
 
-    ; Term already blacklisted
-    (.reply msg "that word is already blacklisted.")))
+      ; Check if term is not blacklist
+      (if-not (contains? blacklist new_term)
+        ; Term not blacklisted
+        (do
+          ; Add term to blacklist and sort alphabetically before writing file
+          (repo/write-data (sort(conj blacklist new_term)))
+          ; Reply with success message
+          (.reply msg "the blacklist has been updated!"))
+        ; Term already blacklisted
+        (.reply msg "that word is already blacklisted.")))
+
+    ; Send error if not properly formatted
+    (.reply msg "Incorrect command format. Command should be '.addterm =<term>'")))
 
 
 (defn remove_term
+  "When called with '.removeterm =<term>' this function will remove the given <term>
+  from the blacklist if it's currently blacklisted"
   [msg]
+
   ;Load blacklist
   (def blacklist (into #{} (repo/read_blacklist)))
 
-  ; Hardcoded test
-  ;(def msgt ".removeterm =Chocolate")
-  ;(def content (str/lower-case msgt))
-  ;(println content)
-
   ; Get content from msg
-  (def content (str/lower-case (aget msg "content")))
+  (def content (re-find #"=\S+" (str/lower-case (aget msg "content"))))
 
-  ; Find term to be added from message content
-  (def new_term (str/replace (re-find #"=\S+" content) #"=" ""))
-
-  ; Check if term is not blacklist
-  (if (contains? blacklist new_term)
-    ; Term blacklisted
+  (if (some? content)
+    ; If command is properly formatted
     (do
-      ; Remove term from blacklist and sort alphabetically before writing file
-      (repo/write-data (sort (disj blacklist new_term)))
-      ; Reply with success message
-      (.reply msg "the blacklist has been updated!"))
+      ; Strip old term down to word only
+      (def old_term (str/replace content #"=" ""))
 
-    ; Term not blacklisted
-    (.reply msg "that word is not currently blacklisted.")))
+      ; Check if term is not blacklist
+      (if (contains? blacklist old_term)
+        ; Term blacklisted
+        (do
+          ; Remove term from blacklist and sort alphabetically before writing file
+          (repo/write-data (sort (disj blacklist old_term)))
+          ; Reply with success message
+          (.reply msg "the blacklist has been updated!"))
+        ; Term not blacklisted
+        (.reply msg "that word is not currently blacklisted.")))
+
+    ; Send error if not properly formatted
+    (.reply msg "Incorrect command format. Command should be '.removeterm =<term>'")))
 
 
 (defn view_terms
