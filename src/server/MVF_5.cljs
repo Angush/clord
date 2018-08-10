0b501be5b78dc9d60483b9c99e9165d386558c18
Shane <40532070+Shane-West@users.noreply.github.com>
2018-08-11 09:35:04 +1000

    Update MVF_5.cljs
    Changed from '.addterm =<word>' to '.addterm <word>'

--- src/server/MVF_5.cljs
+++ src/server/MVF_5.cljs
@@ -6,7 +6,7 @@
             [goog.string.format]))
 
 (defn add_term
-  "When called with '.addterm =<term>' this function will add the given <term> to
+  "When called with '.addterm <term>' this function will add the given <term> to
    the blacklist if it's not currently blacklisted"
   [bot msg]
 
@@ -14,13 +14,13 @@
   (def blacklist (into #{} (repo/read_blacklist)))
 
   ; Get content from msg
-  (def content (re-find #"=\S+" (str/lower-case (aget msg "content"))))
+  (def content (re-find #" \S+" (str/lower-case (aget msg "content"))))
 
   (if (some? content)
     ; If command is properly formatted
     (do
       ; Strip new term down to word only
-      (def new_term (str/replace content #"=" ""))
+      (def new_term (str/replace content #" " ""))
 
       ; Check if term is not blacklist
       (if-not (contains? blacklist new_term)
@@ -28,17 +28,18 @@
         (do
           ; Add term to blacklist and sort alphabetically before writing file
           (repo/write-blacklist (sort(conj blacklist new_term)))
+
           ; Reply with success message
           (.reply msg "the blacklist has been updated!"))
+
         ; Term already blacklisted
         (.reply msg "that word is already blacklisted.")))
 
     ; Send error if not properly formatted
-    (.reply msg "Incorrect command format. Command should be '.addterm =<term>'")))
-
+    (.reply msg "Incorrect command format. Command should be '.addterm <term>'")))
 
 (defn remove_term
-  "When called with '.removeterm =<term>' this function will remove the given <term>
+  "When called with '.removeterm <term>' this function will remove the given <term>
   from the blacklist if it's currently blacklisted"
   [bot msg]
 
@@ -46,15 +47,15 @@
   (def blacklist (into #{} (repo/read_blacklist)))
 
   ; Get content from msg
-  (def content (re-find #"=\S+" (str/lower-case (aget msg "content"))))
+  (def content (re-find #" \S+" (str/lower-case (aget msg "content"))))
 
   (if (some? content)
     ; If command is properly formatted
     (do
       ; Strip old term down to word only
-      (def old_term (str/replace content #"=" ""))
+      (def old_term (str/replace content #" " ""))
 
-      ; Check if term is not blacklist
+      ; Check if term is blacklist
       (if (contains? blacklist old_term)
         ; Term blacklisted
         (do
@@ -66,8 +67,7 @@
         (.reply msg "that word is not currently blacklisted.")))
 
     ; Send error if not properly formatted
-    (.reply msg "Incorrect command format. Command should be '.removeterm =<term>'")))
-
+    (.reply msg "Incorrect command format. Command should be '.removeterm <term>'")))
 
 (defn view_terms
   "Sends a message containing the blacklist as a reply to .viewterms"
