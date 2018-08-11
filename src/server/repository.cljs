b769fe79dc4116b9ce71378f6b91a6f23173f196
Shane <40532070+Shane-West@users.noreply.github.com>
2018-08-11 15:03:40 +1000

    Update repository.cljs

--- src/server/repository.cljs
+++ src/server/repository.cljs
@@ -1,34 +1,47 @@
 (ns server.repository
   (:require ["fs" :as fs]))
 
-(def filename "../src/db.json")
+(def filename "src/db.json")
 (def blacklist "src/blacklist.json")
 
 (defn read-db
-      []
-      (js->clj (.parse js/JSON (fs/readFileSync filename "utf8")) :keywordize-keys true))
-
+  []
+  (js->clj (.parse js/JSON (fs/readFileSync filename "utf8")) :keywordize-keys true))
 
 (defn read_blacklist
-      []
-      (js->clj (.parse js/JSON (fs/readFileSync blacklist "utf8")) :keywordize-keys true))
+  []
+  (js->clj (.parse js/JSON (fs/readFileSync blacklist "utf8")) :keywordize-keys true))
 
+(def write-blacklist
+  "Write data to the DB file. This function assumes next-state contains
+  all the data or the full db"
+  (comp
+    #(fs/writeFileSync blacklist %)
+    #(.stringify js/JSON %)
+    clj->js))
+	
+(def write-data
+  "Write data to the DB file. This function assumes next-state contains
+  all the data or the full db"
+  (comp
+    #(fs/writeFileSync filename %)
+    #(.stringify js/JSON %)
+    clj->js))
 
+(defn get-user-rap-by-id
+  "Find user's raphseet by id. If the user is not found
+  the function returns nil."
+  [id]
+  (let [rapsheet (:rapsheet (read-db))]
+    (get rapsheet (keyword id))))
 
-(defn write-data
-      "Write data to the DB file. This function assumes next-state contains
-      all the data or the full db"
-      [next-state]
-      (->>
-        next-state
-        (clj->js)
-        #(.stringify js/JSON %)
-        (partial fs/writeFileSync filename)))
-
-(defn get-users
-      []
-      (:users (read-db)))
-
-(defn add-user
-      []
-      (write-data {:users [{:name "Daniel"} {:name "Angus"}]}))
+(defn update-user-rapsheet
+  "Update user rapsheet"
+  [id key val]
+  (let [db (read-db)
+        rapsheet (:rapsheet db)
+        user (get rapsheet (keyword id))
+        updated-user (assoc user key val)
+        updated-sheet (assoc rapsheet (keyword id) updated-user)
+        updated-db (assoc db :rapsheet updated-sheet)]
+    (write-data updated-db)))
