7873b8d315734115fea931d19b603af589055f2f
Shane <40532070+Shane-West@users.noreply.github.com>
2018-08-11 15:02:50 +1000

    Update discord_handler.cljs
    Contains updated requirements from dev branch

--- src/server/discord_handler.cljs
+++ src/server/discord_handler.cljs
@@ -1,5 +1,6 @@
 (ns server.discord-handler
-  (:require [server.mvf4 :refer [punish]]
+  (:require [server.mvf3 :refer [rapsheet]]
+            [server.mvf4 :refer [punish]]
             [server.MVF_5 :refer [add_term]]
             [server.MVF_5 :refer [remove_term]]
             [server.MVF_5 :refer [view_terms]]))
@@ -15,16 +16,20 @@
     :exec    (fn [msg-obj] (.reply msg-obj "world"))
     }
    {
+    :command ".rapsheet"
+    :exec    rapsheet
+    }
+   {
     :command ".addterm"
-    :exec    (fn [msg-obj] (add_term msg-obj))
+    :exec    add_term
     }
    {
     :command ".removeterm"
-    :exec    (fn [msg-obj] (remove_term msg-obj))
+    :exec    remove_term
     }
    {
     :command ".viewterms"
-    :exec    (fn [msg-obj] (view_terms msg-obj))
+    :exec    view_terms
     }
    ]
   )
@@ -38,9 +43,9 @@
 (defn handle-command
   "This function checks the message and when it matches a
   command it executes a function provided"
-  [msg]
+  [bot msg]
   (let [msg-content (aget msg "content")
         command (find-command msg-content)]
        (if (nil? command)
          msg
-         ((:exec command) msg))))
+         ((:exec command) bot msg))))
