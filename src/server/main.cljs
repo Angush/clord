005b2d4a68bde386e8756e7c2df9e6b1fd709b6b
Angush (PC) <angus.stienlehmann@gmail.com>
2018-08-16 21:46:46 +1000

    Added error reporting for invalid environment data and failed logins with process termination, added message flagging event, and removed second message event. (Note: MVF1 will be broken until I commit the edited discord_handler)

--- src/server/main.cljs
+++ src/server/main.cljs
@@ -1,7 +1,9 @@
 (ns server.main
   (:require ["discord.js" :as discord]
             [server.discord-handler :refer [handle-command]]
-            [server.environment :refer [env-vars]]))
+            [server.environment :refer [env-vars]]
+            [server.message-flagging :refer [check-reaction]]
+            [server.environment :refer [validate-env-vars]]))
 
 ; Discord configuration
 (def token (get env-vars "DISCORD_TOKEN"))
@@ -10,18 +12,38 @@
 ; Discord client event listeners
 (.on discord-client
      "ready"
-     (partial println "Discord client connected."))
+     (partial println "Ready to go!"))
 
 (.on discord-client
      "message"
      (partial handle-command discord-client))
+
+(.on discord-client
+     "messageReactionAdd"
+     (partial check-reaction discord discord-client))
 
 ; App lifecycle events
 (defn reload! []
       (do
         (println "Code updated!")))
 
+(defn successful-login []
+  (println "Login succeeded. Validating data...")
+  (let [user (aget discord-client "user")
+        valid (validate-env-vars discord-client user)]
+    (if (true? valid)
+      (let [tag (aget user "tag")]
+        (println (str "Everything looks OK! Initialized as " tag ".")))
+      (do
+        (println "Fix this and run me again. Shutting down...")
+        (js/process.exit)))))
+
+(defn failed-login
+  [err]
+  (println (str "Login failed. ERROR:\n" err "\n\nNOTE: If this is about incorrect login details, you may have set up your .env file incorrectly. Ensure the file is named '.env' exactly, and not something else like '.env.skell', or I won't be able to load properly. If that isn't the issue, ensure my .env file contains a valid token for a Discord bot app.")))
+
 (defn main! []
       (do
         (println "App loaded!")
-        (.login discord-client token)))
+        (let [login-promise (.login discord-client token)]
+        (.then login-promise successful-login failed-login))))
